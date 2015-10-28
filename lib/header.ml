c4ad17656b76883bcf6945eed0975b26aeeaaaaa
David Scott <dave.scott@unikernel.com>
2015-10-28 20:59:26 +0000

    Support writing a qcow2 header
    This is a transliteration of the spec:
    
    https://people.gnome.org/~markmc/qcow-image-format.html
    
    Signed-off-by: David Scott <dave.scott@unikernel.com>

--- lib/header.ml
+++ lib/header.ml
@@ -14,5 +14,112 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
  *)
+open Sexplib.Std
 open Result
+open Types
+open Error
 
+module Version = struct
+  type t = [
+    | `One
+    | `Two
+  ] with sexp
+
+
+  let sizeof t = 4
+  
+  let write t rest =
+    Int32.write (match t with | `One -> 1l | `Two -> 2l) rest
+
+  let read rest =
+    Int32.read rest
+    >>= fun (version, rest) ->
+    match version with
+    | 1l -> return (`One, rest)
+    | 2l -> return (`Two, rest)
+    | _ -> error_msg "Unknown version: %ld" version
+end
+
+module CryptMethod = struct
+
+  type t = [ `Aes | `None ] with sexp
+
+  let sizeof _ = 4
+
+  let write t rest =
+    Int32.write (match t with | `Aes -> 1l | `None -> 0l) rest
+
+  let read rest =
+    Int32.read rest
+    >>= fun (m, rest) ->
+    match m with
+    | 0l -> return (`None, rest)
+    | 1l -> return (`Aes, rest)
+    | _ -> error_msg "Unknown crypt_method: %ld" m
+end
+
+type offset = int64 with sexp
+
+type t = {
+  version: Version.t;
+  backing_file_offset: offset;
+  backing_file_size: int32;
+  backing_file: string option;
+  cluster_bits: int32;
+  size: int64;
+  crypt_method: CryptMethod.t;
+  ll_size: int32;
+  ll_table_offset: offset;
+  refcount_table_offset: offset;
+  refcount_table_clusters: int32;
+  nb_snapshots: int32;
+  snapshots_offsets: offset;
+} with sexp
+
+let sizeof _ = 4 + 4 + 8 + 4 + 4 + 8 + 4 + 4 + 8 + 8 + 4 + 4 + 8
+
+let write t rest =
+  big_enough_for "Header" rest (sizeof t)
+  >>= fun () ->
+  Int8.write (int_of_char 'Q') rest
+  >>= fun rest ->
+  Int8.write (int_of_char 'F') rest
+  >>= fun rest ->
+  Int8.write (int_of_char 'I') rest
+  >>= fun rest ->
+  Int8.write 0xfb rest
+  >>= fun rest ->
+  Version.write t.version rest
+  >>= fun rest ->
+  Int64.write t.backing_file_offset rest
+  >>= fun rest ->
+  Int32.write t.backing_file_size rest
+  >>= fun rest ->
+  Int32.write t.cluster_bits rest
+  >>= fun rest ->
+  Int64.write t.size rest
+  >>= fun rest ->
+  CryptMethod.write t.crypt_method rest
+  >>= fun rest ->
+  Int32.write t.ll_size rest
+  >>= fun rest ->
+  Int64.write t.ll_table_offset rest
+  >>= fun rest ->
+  Int64.write t.refcount_table_offset rest
+  >>= fun rest ->
+  Int32.write t.refcount_table_clusters rest
+  >>= fun rest ->
+  Int32.write t.nb_snapshots rest
+  >>= fun rest ->
+  Int64.write t.snapshots_offsets rest
+
+let read rest =
+ big_enough_for "Header" rest (sizeof ())
+ >>= fun () ->
+ Int8.read rest
+ >>= fun (x, rest) ->
+ ( if char_of_int x = 'Q'
+   then return rest
+   else error_msg "Expected magic: got %02x" x )
+ >>= fun rest ->
+ failwith "unimplemented"
--- lib/header.mli
+++ lib/header.mli
@@ -15,3 +15,39 @@
  *
  *)
 
+module Version : sig
+  type t = [
+    | `One
+    | `Two
+  ] with sexp
+
+  include S.SERIALISABLE with type t := t
+end
+
+module CryptMethod : sig
+  type t = [ `Aes | `None ] with sexp
+
+  include S.SERIALISABLE with type t := t
+end
+
+type offset = int64
+(** Offset within the image *)
+
+type t = {
+  version: Version.t;
+  backing_file_offset: offset;        (** offset of the backing file path *)
+  backing_file_size: int32;           (** length of the backing file path *)
+  backing_file: string option;        (** a path, if this is a copy-on-write image *)
+  cluster_bits: int32;                (** a cluster is 2 ** cluster_bits in size *)
+  size: int64;                        (** virtual size of the image *)
+  crypt_method: CryptMethod.t;
+  ll_size: int32;                     (** number of 8-byte entries in the L1 table *)
+  ll_table_offset: offset;            (** offset of the L1 table *)
+  refcount_table_offset: offset;      (** offset of the refcount table *)
+  refcount_table_clusters: int32;     (** size of the refcount table in clusters *)
+  nb_snapshots: int32;                (** the number of internal snapshots *)
+  snapshots_offsets: offset;          (** offset of the snapshot header *)
+} with sexp
+(** The qcow2 header *)
+
+include S.SERIALISABLE with type t := t
