fb54fbdfe61873b791f3f1ee888ded94f37e2d2c
David Scott <dave.scott@unikernel.com>
2015-10-28 19:28:15 +0000

    Import a subset of the `module Types` from 9P
    Signed-off-by: David Scott <dave.scott@unikernel.com>

---
+++ lib/types.mli
@@ -0,0 +1,53 @@
+(*
+ * Copyright (C) 2015 David Scott <dave@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
+(** Parsers and printers for types used in qcow2 fields *)
+
+open Sexplib
+
+val big_enough_for: string -> Cstruct.t -> int -> unit Error.t
+(** [big_enough_for name buf length] returns an error with a log message
+    if buffer [buf] is smaller than [length]. The [name] will be included
+    in the error message. *)
+
+module Int8 : sig
+  type t = int with sexp
+
+  include S.SERIALISABLE with type t := t
+end
+
+module Int16 : sig
+  type t = int with sexp
+
+  include S.SERIALISABLE with type t := t
+end
+
+module Int32 : sig
+  include module type of Int32
+
+  val t_of_sexp: Sexp.t -> t
+  val sexp_of_t: t -> Sexp.t
+
+  include S.SERIALISABLE with type t := t
+end
+
+module Int64 : sig
+  type t = int64 with sexp
+
+  include S.SERIALISABLE with type t := t
+end
+
