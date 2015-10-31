1857e14b4eb12d6cd5b89eb9de67d91548ef41dd
David Scott <dave.scott@unikernel.com>
2015-10-31 11:54:54 +0000

    test: check a header for a small file
    Signed-off-by: David Scott <dave.scott@unikernel.com>

--- lib_test/test.ml
+++ lib_test/test.ml
@@ -17,7 +17,24 @@ open Qcow
 open Lwt
 open OUnit
 
-let create () =
+(* qemu-img will set version = `Three and leave an extra cluster
+   presumably for extension headers *)
+
+let create_1K () =
+  let module B = Qcow.Client.Make(Block) in
+  let hdr = B.create () 1024L in
+  let expected = {
+    Header.version = `Two; backing_file_offset = 0L;
+    backing_file_size = 0l; cluster_bits = 16l; size = 1024L;
+    crypt_method = `None; l1_size = 1l; l1_table_offset = 131072L;
+    refcount_table_offset = 65536L; refcount_table_clusters = 1l;
+    nb_snapshots = 0l; snapshots_offset = 0L
+  } in
+  let cmp a b = Header.compare a b = 0 in
+  let printer = Header.to_string in
+  assert_equal ~printer ~cmp expected hdr
+
+let create_1M () =
   let module B = Qcow.Client.Make(Block) in
   let hdr = B.create () 1048576L in
   (* qemu-img will set version = `Three and leave an extra cluster
@@ -31,12 +48,11 @@ let create () =
   } in
   let cmp a b = Header.compare a b = 0 in
   let printer = Header.to_string in
-  assert_equal ~printer ~cmp expected hdr;
-  let _ = B.create () 1048576L in
-  ()
+  assert_equal ~printer ~cmp expected hdr
 
 let _ =
   let suite = "qcow2" >::: [
-    "create" >:: create;
+    "create 1K" >:: create_1K;
+    "create 1M" >:: create_1M;
   ] in
   OUnit2.run_test_tt_main (ounit2_of_ounit1 suite)
