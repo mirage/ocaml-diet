3604763d6d222e92164ee80e64775fee7a638e7e
David Scott <dave@recoil.org>
2016-12-08 11:50:26 +0000

    Move the Qcow_diet signature to Qcow_s
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/qcow_diet.mli
+++ lib/qcow_diet.mli
@@ -28,70 +28,7 @@ module type ELT = sig
   (** Successor of an element *)
 end
 
-module Make(Elt: ELT): sig
-  (** A discrete interval encoding tree: a set of elements optimised for cases
-      where large numbers of contiguous elements are in the set. *)
-
-  type elt = Elt.t [@@deriving sexp]
-  (** The type of the set elements *)
-
-  type interval
-  (** An interval: a range (x, y) of set values where all the elements from
-      x to y inclusive are in the set *)
-
-  module Interval: sig
-    val make: elt -> elt -> interval
-    (** [make first last] construct an interval describing all the elements from
-        [first] to [last] inclusive. *)
-
-    val x: interval -> elt
-    (** the starting element of the interval *)
-
-    val y: interval -> elt
-    (** the ending element of the interval *)
-  end
-
-  type t [@@deriving sexp]
-  (** The type of sets *)
-
-  val empty: t
-  (** The empty set *)
-
-  val is_empty: t -> bool
-  (** Test whether a set is empty or not *)
-
-  val mem: elt -> t -> bool
-  (** [mem elt t] tests whether [elt] is in set [t] *)
-
-  val fold: (interval -> 'a -> 'a) -> t -> 'a -> 'a
-  (** [fold f t acc] folds [f] across all the intervals in [t] *)
-
-  val add: interval -> t -> t
-  (** [add interval t] returns the set consisting of [t] plus [interval] *)
-
-  val remove: interval -> t -> t
-  (** [remove interval t] returns the set consisting of [t] minus [interval] *)
-
-  val min_elt: t -> interval
-  (** [min_elt t] returns the smallest (in terms of the ordering) interval in
-      [t], or raises [Not_found] if the set is empty. *)
-
-  val max_elt: t -> interval
-  (** [max_elt t] returns the largest (in terms of the ordering) interval in
-      [t], or raises [Not_found] if the set is empty. *)
-
-  val choose: t -> interval
-  (** [choose t] returns one interval, or raises Not_found if the set is empty *)
-
-  val union: t -> t -> t
-  (** set union *)
-
-  val diff: t -> t -> t
-  (** set difference *)
-
-  val inter: t -> t -> t
-  (** set intersection *)
-end
+module Make(Elt: ELT): Qcow_s.INTERVAL_SET with type elt = Elt.t
 
 module Test: sig
   val all: (string * (unit -> unit)) list
