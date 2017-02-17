858974bb2fa89fac5751e215d4c699d9db705579
David Scott <dave@recoil.org>
2017-02-17 11:06:35 +0000

    locking: always increase the refcount in get_lock
    The previous bugfix missed the bug: on every path through get_lock
    the refcount should be bumped. When a lock is created it has no references
    it has a count of 0 and then is bumped to 1 by get_lock.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/qcow_cluster.ml
+++ lib/qcow_cluster.ml
@@ -43,9 +43,9 @@ let get_lock t cluster =
     if Int64Map.mem cluster t.locks
     then Int64Map.find cluster t.locks
     else begin
-      Qcow_rwlock.make (), 1
+      Qcow_rwlock.make (), 0
     end in
-  t.locks <- Int64Map.add cluster (lock, refcount) t.locks;
+  t.locks <- Int64Map.add cluster (lock, refcount + 1) t.locks;
   lock
 
 let put_lock t cluster =
