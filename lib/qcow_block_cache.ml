cc3d38e4e966bf7c02675723e0cbae0143749667
David Scott <dave@recoil.org>
2017-02-17 15:25:00 +0000

    cache: limit the size of the buffer fragment list
    Previously we would generate a very large list of buffer fragments to
    write in some cases. This patch limits each list to covering 1MiB of data.
    
    Note there is no need to cache the "current size" of the cache since the
    `cardinal t.in_cache` gives the authoritative value.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/qcow_block_cache.ml
+++ lib/qcow_block_cache.ml
@@ -15,6 +15,13 @@
  *
  *)
 
+let src =
+  let src = Logs.Src.create "qcow" ~doc:"qcow2-formatted BLOCK device" in
+  Logs.Src.set_level src (Some Logs.Info);
+  src
+
+module Log = (val Logs.src_log src : Logs.LOG)
+
 let kib = 1024L
 let mib = Int64.mul kib 1024L
 
@@ -67,7 +74,6 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
     mutable info: Mirage_block.info;
     sector_size: int;
     max_size_bytes: int64;
-    mutable current_size_bytes: int64;
     mutable in_cache: Int64.IntervalSet.t;
     mutable zeros: Int64.IntervalSet.t;
     mutable cache: Cstruct.t Int64.Map.t;
@@ -81,8 +87,13 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
   let get_info t = Lwt.return t.info
 
   let lazy_write_back t =
+    let open Lwt.Infix in
     Lwt_mutex.with_lock t.write_back_m
       (fun () ->
+        Log.debug (fun f -> f "lazy_write_back cached sectors = %Ld zeros = %Ld"
+          (Int64.IntervalSet.cardinal t.in_cache) (Int64.IntervalSet.cardinal t.zeros)
+        );
+        assert (Int64.IntervalSet.(is_empty @@ inter t.in_cache t.zeros));
         (* coalesce known-zeros together with data blocks *)
         let all = Int64.IntervalSet.union t.in_cache t.zeros in
         Int64.IntervalSet.fold_s
@@ -92,21 +103,31 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
               RangeLocks.with_lock t.locks i
                 (fun () ->
                   let x, y = Int64.IntervalSet.Interval.(x i, y i) in
-                  let rec bufs acc sector =
-                    if sector > y then List.rev acc else begin
-                      let buf =
-                        if Int64.Map.mem sector t.cache then begin
-                          let buf = Int64.Map.find sector t.cache in
-                          t.in_cache <- Int64.IntervalSet.remove i t.in_cache;
-                          t.cache <- Int64.Map.remove sector t.cache;
-                          buf
-                        end else t.zero in
-                      bufs (buf :: acc) (Int64.succ sector)
+                  let mib = Int64.(div 1048576L (of_int t.sector_size)) in
+                  (* split the interval into 1MiB chunks *)
+                  let rec loop x y =
+                    if x > y then Lwt.return (Ok ()) else begin
+                      let y' = min (Int64.add x mib) y in
+                      let rec bufs acc sector last =
+                        if sector > last then List.rev acc else begin
+                          let buf =
+                            if Int64.Map.mem sector t.cache then begin
+                              let buf = Int64.Map.find sector t.cache in
+                              t.in_cache <- Int64.IntervalSet.remove i t.in_cache;
+                              t.zeros <- Int64.IntervalSet.remove i t.zeros;
+                              t.cache <- Int64.Map.remove sector t.cache;
+                              buf
+                            end else t.zero in
+                          bufs (buf :: acc) (Int64.succ sector) last
+                        end in
+                      let bufs = bufs [] x y' in
+                      B.write t.base x bufs
+                      >>= function
+                      | Error e -> Lwt.return (Error e)
+                      | Ok () ->
+                        loop (Int64.succ y') y
                     end in
-                  let bufs = bufs [] x in
-                  let len = Int64.of_int @@ Cstructs.len bufs in
-                  t.current_size_bytes <- Int64.sub t.current_size_bytes len;
-                  B.write t.base x bufs
+                  loop x y
                 )
           ) all (Ok ())
       )
@@ -124,7 +145,6 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
     B.get_info base
     >>= fun info ->
     let sector_size = info.Mirage_block.sector_size in
-    let current_size_bytes = 0L in
     let in_cache = Int64.IntervalSet.empty in
     let zeros = Int64.IntervalSet.empty in
     let cache = Int64.Map.empty in
@@ -135,7 +155,7 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
     let zero = Cstruct.create sector_size in
     Cstruct.memset zero 0;
     let t = {
-      base; info; sector_size; max_size_bytes; current_size_bytes; in_cache; cache;
+      base; info; sector_size; max_size_bytes; in_cache; cache;
       zeros; locks; disconnect_request; disconnect_m; write_back_m; zero;
     } in
     Lwt.return t
@@ -197,7 +217,8 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
   let write t start bufs =
     let open Lwt.Infix in
     let len = Int64.of_int @@ Cstructs.len bufs in
-    ( if Int64.(add t.current_size_bytes len) > t.max_size_bytes
+    let current_size_bytes = Int64.(mul (IntervalSet.cardinal t.in_cache) (of_int t.sector_size)) in
+    ( if Int64.(add current_size_bytes len) > t.max_size_bytes
       then lazy_write_back t
       else Lwt.return (Ok ()) )
     >>= function
@@ -216,7 +237,7 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
                   assert (Cstruct.len buf = t.sector_size);
                   if not(Int64.Map.mem sector t.cache) then begin
                     t.in_cache <- Int64.IntervalSet.(add i t.in_cache);
-                    t.current_size_bytes <- Int64.(add t.current_size_bytes (of_int t.sector_size));
+                    t.zeros <- Int64.IntervalSet.(remove i t.zeros);
                   end;
                   t.cache <- Int64.Map.add sector buf t.cache;
                   Lwt.return (Ok ())
@@ -234,13 +255,12 @@ module Make(B: Qcow_s.RESIZABLE_BLOCK) = struct
          size *)
       if new_size < t.info.Mirage_block.size_sectors then begin
         let still_ok, to_drop = Int64.Map.partition (fun sector _ -> sector < new_size) t.cache in
-        let to_drop', nsectors = Int64.Map.fold (fun sector _ (set, count) ->
+        let to_drop' = Int64.Map.fold (fun sector _ set ->
           let i = Int64.IntervalSet.Interval.make sector sector in
-          Int64.IntervalSet.(add i set), count + 1
-        ) to_drop (Int64.IntervalSet.empty, 0) in
+          Int64.IntervalSet.(add i set)
+        ) to_drop Int64.IntervalSet.empty in
         t.cache <- still_ok;
         t.in_cache <- Int64.IntervalSet.diff t.in_cache to_drop';
-        t.current_size_bytes <- Int64.(sub t.current_size_bytes (mul (of_int t.sector_size) (of_int nsectors));)
       end;
       (* If the file has become bigger, we know the new blocks contain zeroes *)
       if new_size > t.info.Mirage_block.size_sectors then begin
