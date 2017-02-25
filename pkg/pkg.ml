dada918936417f883d0b937a631e53ee3259164f
David Scott <dave@recoil.org>
2017-02-25 11:38:05 +0000

    test: execute fewer iterations on travis
    Previously the job was hittng the global test timeout.
    
    Signed-off-by: David Scott <dave@recoil.org>

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -9,5 +9,5 @@ let () =
        Pkg.bin "cli/main" ~dst:"qcow-tool";
        Pkg.test "lib_test/test" ~args:Cmd.(v "-runner" % "sequential");
        Pkg.test "lib_test/compact_random";
-       Pkg.test "lib_test/compact_random" ~args:Cmd.(v "-compact-mid-write");
+       Pkg.test "lib_test/compact_random" ~args:Cmd.(v "-compact-mid-write" % "-stop-after" % "16");
   ]
