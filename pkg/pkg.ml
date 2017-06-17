b84d4dca72e6b17cf8c2374479c4333074ac42e6
David Scott <dave@recoil.org>
2017-06-17 14:04:36 +0100

    Add topkg-jbuilder
    Signed-off-by: David Scott <dave@recoil.org>

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,13 +1,3 @@
 #!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "qcow" @@ fun c ->
-  Ok [ Pkg.mllib "lib/qcow.mllib";
-       Pkg.bin "cli/main" ~dst:"qcow-tool";
-       Pkg.test "lib_test/test" ~args:Cmd.(v "-runner" % "sequential");
-       Pkg.test "lib_test/compact_random";
-       Pkg.test "lib_test/compact_random" ~args:Cmd.(v "-compact-mid-write" % "-stop-after" % "16");
-  ]
+#require "topkg-jbuilder.auto"
