d85264c473b4b0a8f4a6316956e805c2dacd5700
David Scott <dave@recoil.org>
2018-02-15 15:56:50 +0000

    Remove unnecessary signatures and Lwt
    Signed-off-by: David Scott <dave@recoil.org>

--- lib/qcow_s.ml
+++ lib/qcow_s.ml
@@ -17,56 +17,6 @@
 
 (** Common signatures used by the library *)
 
-open Result
-
-module type LOG = sig
-  (** Common logging functions *)
-
-  val debug : ('a, unit, string, unit) format4 -> 'a
-  val info  : ('a, unit, string, unit) format4 -> 'a
-  val error : ('a, unit, string, unit) format4 -> 'a
-end
-
-module type SERIALISABLE = sig
-  (** Values which can be read and written *)
-
-  type t
-  (** Instances of this type can be read and written *)
-
-  val sizeof: t -> int
-  (** The size of a buffer needed to hold [t] *)
-
-  val read: Cstruct.t -> (t * Cstruct.t, [ `Msg of string]) result
-  (** Read a [t] from the given buffer and return it, along with the
-      unused remainder of the buffer. If the buffer cannot
-      be parsed then return an error.*)
-
-  val write: t -> Cstruct.t -> (Cstruct.t, [ `Msg of string]) result
-  (** Write a [t] into the given buffer. If the buffer is too small,
-      then return an error. Return the unused remainder of the buffer.*)
-end
-
-module type PRINTABLE = sig
-  (** Values which can be pretty-printed *)
-
-  type t
-  (** Instances of this type can be pretty-printed *)
-
-  val to_string: t -> string
-  (** Produce a pretty human-readable string from a value *)
-end
-
-module type RESIZABLE_BLOCK = sig
-  include Mirage_block_lwt.S
-
-  val resize: t -> int64 -> (unit, write_error) result Lwt.t
-  (** Resize the file to the given number of sectors. *)
-
-  val flush : t -> (unit, write_error) result io
-  (** [flush t] flushes any buffers, if the file has been opened in buffered
-      mode *)
-end
-
 module type INTERVAL_SET = sig
   type elt
   (** The type of the set elements *)
@@ -105,9 +55,6 @@ module type INTERVAL_SET = sig
   val fold: (interval -> 'a -> 'a) -> t -> 'a -> 'a
   (** [fold f t acc] folds [f] across all the intervals in [t] *)
 
-  val fold_s: (interval -> 'a -> 'a Lwt.t) -> t -> 'a -> 'a Lwt.t
-  (** [fold_s f t acc] folds [f] across all the intervals in [t] *)
-
   val fold_individual: (elt -> 'a -> 'a) -> t -> 'a -> 'a
   (** [fold_individual f t acc] folds [f] across all the individual elements of [t] *)
 
@@ -140,24 +87,4 @@ module type INTERVAL_SET = sig
 
   val inter: t -> t -> t
   (** set intersection *)
-end
-
-module type NUM = sig
-  type t
-  val zero: t
-  val pred: t -> t
-  val succ: t -> t
-  val add: t -> t -> t
-  val sub: t -> t -> t
-  val mul: t -> t -> t
-  val div: t -> t -> t
-  val of_int64: int64 -> t
-  val to_int64: t -> int64
-  val of_int: int -> t
-  val to_int: t -> int
-  val to_string: t -> string
-  val shift_left: t -> int -> t
-  val shift_right_logical: t -> int -> t
-  val logor: t -> t -> t
-  val rem: t -> t -> t
 end
